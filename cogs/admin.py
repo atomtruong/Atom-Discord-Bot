1fdf4ef957a23ddb15f258558f5dbbc12813a78c
truongad <truongad@oregonstate.edu>
2022-03-04 15:35:45 -0800

    trying to fix

--- cogs/admin.py
+++ cogs/admin.py
@@ -54,7 +54,6 @@ class AdminCog(commands.Cog, name="Settings Commands", description="These "
 				as file:
 			channel_file = json.load(file)
 		channel_file['channel'] = channel
-		print(channel_file['channel'])
 		with open(r'/app/config/config.json', 'w') as jsonWrite:
 			json.dump(channel_file, jsonWrite, indent=4)
 		await ctx.send(f'I have changed the bot channel to '
