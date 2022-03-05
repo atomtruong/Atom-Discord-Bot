8c6c81c8eb5a57d9feb82ba8bf7a579d2da931d4
truongad <truongad@oregonstate.edu>
2022-03-04 17:14:36 -0800

    print user username as well for DMs

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ async def on_member_join(member):
 @bot.event
 async def on_message(message):
     if message.guild is None and not message.author.bot:
-        print(f"{message.author.mention} User DM: {message.content}")
+        print(f"{message.author.username} User DM: {message.content}")
     else:
         with open("/app/config/config.json", 'r') as file:
             data = json.load(file)
